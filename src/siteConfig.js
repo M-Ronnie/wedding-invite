3eb45cf62d332846599653d8448d1dd2b3f3c39a
M-Ronnie <134044922+M-Ronnie@users.noreply.github.com>
2026-08-13 12:46:22 +0300

    Update siteConfig.js

--- src/siteConfig.js
+++ src/siteConfig.js
@@ -132,7 +132,7 @@ const siteConfig = {
     subtitle:
       "We're so excited to share our special day with you. Capture and share your favorite moments from our wedding here!",
     ctaButton: "Upload Photos",
-    backgroundImage: "/images/homage_page_background.png",
+    backgroundImage: "/https://i.ibb.co/fV2VxFhC/1000748264.jpg",
     showCountdown: true,
   },
 
